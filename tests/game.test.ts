d108fd7e0238da09c121f6c961ead1034d28bf93
Kirill Stytsenko <kirill@styts.com>
2025-04-19 09:04:43 +0200

    Continue debugging Playwright tests

--- tests/game.test.ts
+++ tests/game.test.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test.describe('Core Game Functionality', () => {
 
   test('Can just see the title', async ({ page }) => {
-    await page.goto('http://localhost:3020');
+    await page.goto('/');
     expect(await page.title()).toBe('Game Title');
   });
 
