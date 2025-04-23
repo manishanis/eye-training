ddf562ca6da81c1011bf2320967be426ec5eff21
Kirill Stytsenko <kirill@styts.com>
2025-04-23 10:13:42 +0200

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  // target: 'static',
+  target: 'static',
   app: {
     baseURL: '/eye-training/', // Important for GitHub Pages
     head: {
